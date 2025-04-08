6fc7966b621c6e1441548ab0a42fd7a60a68b599
lymsg <114712935+lymsg@users.noreply.github.com>
2025-04-08 16:02:43 +0900

    메인씬 버튼 클릭 ani수정

--- Team3/Assets/Scripts/StageButton.cs
+++ Team3/Assets/Scripts/StageButton.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 
 public class StageButton : MonoBehaviour
 {
-    public Animator anim;
+    public Animator anim1;
+    public Animator anim2;
+    public Animator anim3;
+
     public int stage;
     public int cardCount;
     public void StageBtn()
     {
-        anim.SetBool("isClick", true);
         GameManager.instance.stage = stage;
         GameManager.instance.cardCount = cardCount;
+        switch (stage)
+        {
+            case 1:
+                anim1.SetBool("isClick", true);
+                anim2.SetBool("isClick", false);
+                anim3.SetBool("isClick", false);
+                break;
+            case 2:
+                anim1.SetBool("isClick", false);
+                anim2.SetBool("isClick", true);
+                anim3.SetBool("isClick", false);
+                break;
+            case 3:
+                anim1.SetBool("isClick", false);
+                anim2.SetBool("isClick", false);
+                anim3.SetBool("isClick", true);
+                break;
+
+        }
     }
 }
