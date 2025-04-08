00eeea4395353eb5909ad56544991b0f03615ad1
lymsg <114712935+lymsg@users.noreply.github.com>
2025-04-08 14:08:19 +0900

    시작씬 애니추가
    메인텍스트 애니
    시작버튼 idle,click 애니
    게임종료 click 애니

--- Team3/Assets/Scripts/Exit.cs
+++ Team3/Assets/Scripts/Exit.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     public void GameEnd()
     {
+        anim.SetBool("isClick", true);
     #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false;
     #else
             Application.Quit();
     #endif
