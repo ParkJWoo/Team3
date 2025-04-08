b5481607a54bc117c6285255847cffe5f70e9b75
lymsg <114712935+lymsg@users.noreply.github.com>
2025-04-08 20:27:55 +0900

    hell
    버튼추가 및 클릭시 시작

---
+++ Team3/Assets/Scripts/HellStart.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellStart : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject levelPanel;
+    public float closeSpeed;
+    public int stage;
+    public int cardCount;
+    public float localTime { get; private set; } = 0.0f;
+
+    public void LetHellStart()
+    {
+        GameManager.instance.stage = stage;
+        GameManager.instance.cardCount = cardCount;
+        GameManager.instance.closeSpeed = closeSpeed;
+        
+        if (GameManager.instance.stage != 0 && GameManager.instance.closeSpeed != 0)
+        {
+            Debug.Log("dd");
+            
+            anim.SetBool("isClick", true);
+            levelPanel.SetActive(false);
+            GameManager.instance.board.gameObject.SetActive(true);
+            GameManager.instance.timeTxt.gameObject.SetActive(true);
+
+            GameManager.instance.time = 0f; // 타이머 초기화
+            GameManager.instance.isGamePlaying = true; // 게임 시작상태
+
+            AudioManager.instance.ResetSpeed(); //피치 리셋 먼저*
+            AudioManager.instance.PlayMusic(); // BGM 이어서 재생
+        }
+    }
+}
