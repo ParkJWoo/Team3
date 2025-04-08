a66b411348f70f0ef201fb599a27522f0b2c2be3
pm2979 <pm29798512@gmail.com>
2025-04-08 15:16:43 +0900

    시작버튼 변경

--- Team3/Assets/Scripts/GameStart.cs
+++ Team3/Assets/Scripts/GameStart.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class GameStart : MonoBehaviour
@@ -8,8 +9,11 @@ public class GameStart : MonoBehaviour
     public GameObject levelPanel;
     public void LetGameStart()
     {
-        levelPanel.SetActive(false);
-        GameManager.instance.board.gameObject.SetActive(true);
-        GameManager.instance.timeTxt.gameObject.SetActive(true);
+        if (GameManager.instance.stage != 0 && GameManager.instance.closeSpeed != 0)
+        {
+            levelPanel.SetActive(false);
+            GameManager.instance.board.gameObject.SetActive(true);
+            GameManager.instance.timeTxt.gameObject.SetActive(true);
+        }
     }
 }
