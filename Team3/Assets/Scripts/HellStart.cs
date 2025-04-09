4c98417482aef485c957a2c01dbc9219a31e83f1
lymsg <114712935+lymsg@users.noreply.github.com>
2025-04-09 09:45:07 +0900

    시간

--- Team3/Assets/Scripts/HellStart.cs
+++ Team3/Assets/Scripts/HellStart.cs
@@ -26,7 +26,7 @@ public class HellStart : MonoBehaviour
             GameManager.instance.board.gameObject.SetActive(true);
             GameManager.instance.timeTxt.gameObject.SetActive(true);
 
-            GameManager.instance.time = 0f; // 타이머 초기화
+            GameManager.instance.time = 30f; // 타이머 초기화
             GameManager.instance.isGamePlaying = true; // 게임 시작상태
 
             AudioManager.instance.ResetSpeed(); //피치 리셋 먼저*
