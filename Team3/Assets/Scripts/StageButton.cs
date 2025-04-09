44a5dae7f71a7ef1467af5b350dda1b7182680a7
mingyeon <74883641+sichelh@users.noreply.github.com>
2025-04-09 10:03:12 +0900

    Edit StageButton

--- Team3/Assets/Scripts/StageButton.cs
+++ Team3/Assets/Scripts/StageButton.cs
@@ -21,26 +21,12 @@ public class StageButton : MonoBehaviour
                 anim1.SetBool("isClick", true);
                 anim2.SetBool("isClick", false);
                 anim3.SetBool("isClick", false);
-                if (GameManager.instance.Clear >= 0)
-                {
-                    GameManager.instance.stage = stage;
-                    GameManager.instance.cardCount = cardCount;
-                }
-                else
-                {
-                    return;
-                }
                 break;
             case 2:
                 anim1.SetBool("isClick", false);
                 anim2.SetBool("isClick", true);
                 anim3.SetBool("isClick", false);
-                if (GameManager.instance.Clear >= 1)
-                {
-                    GameManager.instance.stage = stage;
-                    GameManager.instance.cardCount = cardCount;
-                }
-                else
+                if (GameManager.instance.Clear < 1)
                 {
                     return;
                 }
@@ -49,18 +35,14 @@ public class StageButton : MonoBehaviour
                 anim1.SetBool("isClick", false);
                 anim2.SetBool("isClick", false);
                 anim3.SetBool("isClick", true);
-                if (GameManager.instance.Clear >= 2)
-                {
-                    GameManager.instance.stage = stage;
-                    GameManager.instance.cardCount = cardCount;
-                }
-                else
+                if (GameManager.instance.Clear < 2)
                 {
                     return;
-                }
+                }   
                 break;
-
         }
-        
+        GameManager.instance.stage = stage;
+        GameManager.instance.cardCount = cardCount;
+
     }
 }
